8d35c61c3bb315335d1b82e0af8e7f2f19cfd257
mprisha <128440532+mprisha@users.noreply.github.com>
2023-03-21 23:39:06 -0400

    Update app.py

--- app.py
+++ app.py
@@ -21,6 +21,7 @@ styl = """
 .stApp {
 /* background-image: url("https://www.bioaster.org/composants/uploads/2022/02/Diagnostics-1-1024x1024.jpg") !important; */
 background-size: cover;
+background: #fff !important;
 }
 h1
 {
